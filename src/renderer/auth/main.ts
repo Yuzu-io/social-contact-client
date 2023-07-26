a578f82d0ba3851b2a831d01f236ead0d0ac78e0
taoxingyu <1581523609@qq.com>
2023-07-26 17:10:58 +0800

    feat:引入自定义图标

--- src/renderer/auth/main.ts
+++ src/renderer/auth/main.ts
@@ -6,8 +6,13 @@ import router from './plugins/router'
 import '../../../resources/reset.css'
 // 引入TDesign少量全局样式变量
 import 'tdesign-vue-next/es/style/index.css'
+//  @mdi/font mdi-vue/v3 @mdi/js
+import MDIcon, { config as MDIconConfig } from './plugins/md-icon'
 
 const app = createApp(App)
+
+//  @mdi/font mdi-vue/v3 @mdi/js
+app.use(MDIcon, { icons: MDIconConfig })
 
 app.use(router)
 app.mount('#app')
