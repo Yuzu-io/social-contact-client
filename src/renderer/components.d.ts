51aac2fa19c113a3ef3f431ec778119535ade723
Yuzu.io <1581523609@qq.com>
2023-09-02 19:37:24 +0800

    refactor:重构main-layout组件

--- src/renderer/components.d.ts
+++ src/renderer/components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    MenuList: typeof import('./src/components/MenuList/index.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TAvatar: typeof import('tdesign-vue-next')['Avatar']
@@ -14,5 +15,9 @@ declare module 'vue' {
     TForm: typeof import('tdesign-vue-next')['Form']
     TFormItem: typeof import('tdesign-vue-next')['FormItem']
     TInput: typeof import('tdesign-vue-next')['Input']
+    TInputNumber: typeof import('tdesign-vue-next')['InputNumber']
+    TLink: typeof import('tdesign-vue-next')['Link']
+    TSpace: typeof import('tdesign-vue-next')['Space']
+    UserAvatar: typeof import('./src/components/UserAvatar/index.vue')['default']
   }
 }
--- src/renderer/src/layouts/main/MainLayout.vue
+++ src/renderer/src/layouts/main/MainLayout.vue
@@ -1,20 +1,8 @@
 <template>
   <div class="main">
     <div class="left">
-      <div class="user">
-        <t-avatar>W</t-avatar>
-      </div>
-      <div class="menu-list">
-        <div
-          v-for="(menu, index) in iconList"
-          :key="index"
-          class="menu-list__item"
-          :class="{ 'menu-list__item--active': currentIndex == index }"
-          @click="clickMenu(index)"
-        >
-          <mdicon :name="menu" size="30"></mdicon>
-        </div>
-      </div>
+      <UserAvatar></UserAvatar>
+      <MenuList></MenuList>
     </div>
     <div class="right">
       <router-view />
@@ -23,19 +11,11 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-
-const iconList = ref<string[]>(['message-text', 'account', 'cog'])
-
-const currentIndex = ref<number>(0)
-const clickMenu = (index: number) => {
-  currentIndex.value = index
-}
+import UserAvatar from '@renderer/src/components/UserAvatar/index.vue'
+import MenuList from '@renderer/src/components/MenuList/index.vue'
 </script>
 
 <style lang="scss" scoped>
-$menu-shadow: #e0e1e2;
-
 .main {
   width: 100%;
   height: 100vh;
@@ -48,47 +28,6 @@ $menu-shadow: #e0e1e2;
     width: 60px;
     background-color: #ececec;
     padding: 30px 0 0 0;
-
-    .user {
-      width: 60px;
-      height: 60px;
-      line-height: 60px;
-      text-align: center;
-      margin: 0 0 80px;
-      -webkit-app-region: no-drag;
-    }
-
-    .menu-list {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      -webkit-app-region: no-drag;
-
-      &__item {
-        width: 40px;
-        height: 40px;
-        line-height: 40px;
-        text-align: center;
-        border-radius: 5px;
-        margin: 0 0 10px 0;
-
-        .mdi {
-          color: #bdc9e1;
-        }
-
-        &:hover {
-          background-color: $menu-shadow;
-        }
-      }
-
-      &__item--active {
-        background-color: $menu-shadow;
-
-        .mdi {
-          color: #003cab;
-        }
-      }
-    }
   }
 
   .right {
