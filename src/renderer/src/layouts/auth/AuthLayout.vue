2b97c1e3d4c0a570d544a65174a1c0edc21d2e09
Yuzu.io <1581523609@qq.com>
2023-09-02 19:36:13 +0800

    feat:添加切换路由Transition动画

--- src/renderer/src/layouts/auth/AuthLayout.vue
+++ src/renderer/src/layouts/auth/AuthLayout.vue
@@ -12,7 +12,9 @@
     </div>
     <!-- 内容区 -->
     <div class="content">
-      <router-view />
+      <Transition name="slide-fade">
+        <router-view />
+      </Transition>
     </div>
   </div>
 </template>
@@ -76,5 +78,20 @@ const closeWindow = () => {
       }
     }
   }
+}
+
+//
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
 }
 </style>
