458d7599ef96e8400e99dd714ea7ee7a7b90be60
Yuzu.io <1581523609@qq.com>
2023-09-06 13:44:36 +0800

    refactor:封装会话组件

--- src/renderer/src/views/message/index.vue
+++ src/renderer/src/views/message/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="message">
-    <div class="session-window">
+    <div class="session">
       <div class="search">
         <t-input placeholder="搜索">
           <template #prefix-icon>
@@ -9,19 +9,7 @@
         </t-input>
       </div>
 
-      <div class="session-list">
-        <div v-for="(item, index) in sessionList" :key="index" class="session-list__item">
-          <div class="left">
-            <t-avatar>W</t-avatar>
-          </div>
-          <div class="right">
-            <div class="user-name">
-              {{ item.username }}
-            </div>
-            <div class="latest-news">测试</div>
-          </div>
-        </div>
-      </div>
+      <SessionList></SessionList>
     </div>
     <div class="content">
       <div>
@@ -39,69 +27,15 @@ import TransferUserInfoDataHelper from '@renderer/src/shared/helper/transfer-use
 import ElectronWindowHelper from '@renderer/src/shared/helper/electron-window'
 import { ElectronWindowType } from '@main/window-type'
 import { UserInfo } from '@/types/index'
-import { reactive, ref } from 'vue'
+import { reactive } from 'vue'
 import { onMounted } from 'vue'
+import SessionList from '@renderer/src/components/SessionList/index.vue'
 
 const userInfo = reactive<UserInfo>({
   username: '',
   sex: '',
   token: ''
 })
-
-const sessionList = ref([
-  {
-    id: '1',
-    username: 'yuzu'
-  },
-  {
-    id: '1',
-    username: 'yuzu'
-  },
-  {
-    id: '1',
-    username: 'yuzu'
-  },
-  {
-    id: '1',
-    username: 'yuzu'
-  },
-  {
-    id: '1',
-    username: 'yuzu'
-  },
-  {
-    id: '1',
-    username: 'yuzu'
-  },
-  {
-    id: '1',
-    username: 'yuzu'
-  },
-  {
-    id: '1',
-    username: 'yuzu'
-  },
-  {
-    id: '1',
-    username: 'yuzu'
-  },
-  {
-    id: '1',
-    username: 'yuzu'
-  },
-  {
-    id: '1',
-    username: 'yuzu'
-  },
-  {
-    id: '1',
-    username: 'yuzu'
-  },
-  {
-    id: '1',
-    username: 'yuzu'
-  }
-])
 
 // 切换窗口
 const switchWindow = () => {
@@ -121,57 +55,20 @@ async function demo() {
 </script>
 
 <style lang="scss" scoped>
-$session-shadow: #e7e7e7;
-
 .message {
   display: flex;
   justify-content: flex-start;
   width: 100%;
   height: 100%;
 
-  .session-window {
-    width: 100%;
-    min-width: 200px;
-    max-width: 250px;
+  .session {
+    width: 200px;
     padding: 30px 0 0 0;
+    border-right: 1px solid rgba(0, 0, 0, 0.1);
 
     .search {
       padding: 0 18px 10px;
       -webkit-app-region: no-drag;
-    }
-
-    .session-list {
-      height: calc(100% - 42px);
-      min-height: 428px;
-      overflow-y: scroll;
-      display: flex;
-      flex-direction: column;
-      -webkit-app-region: no-drag;
-
-      &__item {
-        height: 70px;
-        min-height: 70px;
-        padding: 0 18px;
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-
-        &:hover {
-          background-color: $session-shadow;
-        }
-
-        .left {
-          .t-avatar {
-            width: 40px;
-            height: 40px;
-          }
-        }
-
-        .right {
-          flex: 1;
-          margin: 0 0 0 10px;
-        }
-      }
     }
   }
 
